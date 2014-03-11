4b5b8907ddb04bf83a98906e04c67949a30906ab
Florentin Le Moal <florentin.lemoal@gmail.com>
2014-03-11 21:30:16 +0100

    Ajout d'un système pour obtenir la moyenne d'un élève

--- src/model/Eleve.java
+++ src/model/Eleve.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 /**
@@ -11,39 +10,41 @@ package model;
  * @author flemoal
  */
 public class Eleve extends Personne {
+
     private Moyenne[] listeMoyenne;
     private String commentaireGeneral;
-    
+
     public Eleve(String nom, String prenom) {
         super(nom, prenom);
         commentaireGeneral = "";
         listeMoyenne = new Moyenne[Matiere.values().length];
-        for(int i=0;i<listeMoyenne.length;i++)
-            listeMoyenne[i]=new Moyenne();
+        for (int i = 0; i < listeMoyenne.length; i++) {
+            listeMoyenne[i] = new Moyenne();
+        }
     }
-    
-    public float getNote(Matiere m)
-    {
-        if(listeMoyenne[m.ordinal()]!=null)
+
+    public float getNote(Matiere m) {
+        if (listeMoyenne[m.ordinal()] != null) {
             return listeMoyenne[m.ordinal()].getNote();
+        }
         return -1;
     }
-    
-    public String getCommentaire(Matiere m)
-    {
-        if(listeMoyenne[m.ordinal()]!=null)
+
+    public String getCommentaire(Matiere m) {
+        if (listeMoyenne[m.ordinal()] != null) {
             return listeMoyenne[m.ordinal()].getCommentaire();
+        }
         return "";
     }
 
     public String getCommentaireGeneral() {
         return commentaireGeneral;
     }
-    
+
     public void setNote(Matiere m, float note) {
         listeMoyenne[m.ordinal()].setNote(note);
     }
-    
+
     public void setCommentaire(Matiere m, String com) {
         listeMoyenne[m.ordinal()].setCommentaire(com);
     }
@@ -51,6 +52,24 @@ public class Eleve extends Personne {
     public void setCommentaireGeneral(String commentaireGeneral) {
         this.commentaireGeneral = commentaireGeneral;
     }
-    
-    
+
+    public float getMoyenne() {
+        float sommeNote = 0;
+        float nbNote = 0;
+
+        for (Moyenne m : listeMoyenne) {
+            if (m.getNote() != -1) {
+                sommeNote += m.getNote();
+                nbNote++;
+            }
+        }
+        
+        if (nbNote != 0) {
+            return sommeNote / nbNote;
+        } else {
+            return -1;
+        }
+
+    }
+
 }
